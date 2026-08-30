af4fe66be2bc8e05dfb4a517b4728cd05c23992a
arturbezerra1 <arturbezerra916@gmail.com>
2026-08-30 12:36:48 -0300

    Adiciona questao 1

---
+++ questao1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+
+    printf("Entre com o valor: ");
+    scanf("%d", &x);
+
+    printf("Hexadecimal: %X\n", x);
+    printf("Octal: %o\n", x);
+
+    return 0;
+}
