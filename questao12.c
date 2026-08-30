313bc894520c5e76244a4791656e151554b00e65
arturbezerra1 <arturbezerra916@gmail.com>
2026-08-30 16:20:22 -0300

    Adiciona questao 12

---
+++ questao12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, temp;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &A);
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &B);
+
+    temp = A;
+    A = B;
+    B = temp;
+
+    printf("\n--- Valores apos a troca ---\n");
+    printf("A: %d\n", A);
+    printf("B: %d\n", B);
+
+    return 0;
+}
