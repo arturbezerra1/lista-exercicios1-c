2154a246aa43174b9fdb914b4197192226557c1a
arturbezerra1 <arturbezerra916@gmail.com>
2026-08-30 20:26:02 -0300

    Adiciona questao 24

---
+++ questao24.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int total_segundos, horas, minutos, segundos, resto;
+
+    printf("Digite o tempo em segundos: ");
+    scanf("%d", &total_segundos);
+
+    horas = total_segundos / 3600;
+    
+    resto = total_segundos % 3600;
+
+    minutos = resto / 60;
+
+    segundos = resto % 60;
+
+    printf("%d segundos equivalem a: %d h, %d min e %d s\n", total_segundos, horas, minutos, segundos);
+
+    return 0;
+}
